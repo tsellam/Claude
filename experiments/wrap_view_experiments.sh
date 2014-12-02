aecd95e058bebcfaf1f09e749ad5ba5387f71665
Thibault Sellam <sellam@cwi.nl>
2014-12-02 21:28:14 +0100

    Added experiments wrapper for the cluster

---
+++ experiments/wrap_view_experiments.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd ../data/experiments
+./get_data.sh
+
+mkdir KEEP
+case $machine in
+	rocks138)
+        mv adult.arff communities.arff diabetes.arff KEEP
+	;;
+	rocks139)
+        mv insurance.arff liver.arff shape.arff KEEP
+	;;
+	rocks140)
+        mv breast.arff cover_type.arff glass.arff KEEP
+	;;
+	rocks141)
+        mv internet_usage.arff pendigits.arff vowel.arff KEEP
+	;;
+esac
+rm *.arff
+mv KEEP/* .
+
+cd ../../experiments
+./StrengthTest.R
+./ViewSelection.R
+
+tarname=FindView-`hostname -s`-` date +'%B%d'`.tar.gz
+tar -czvf $tarname nohup.out *.csv *.out *.log
+scp $tarname sellam@warsaw.ins.cwi.nl:~
