2fc0e6e18d52f56b7bc21b8830936ca646945a15
Thibault Sellam <sellam@cwi.nl>
2014-12-03 13:18:49 +0100

    More experiments settings

--- experiments/wrap_view_experiments.sh
+++ experiments/wrap_view_experiments.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-#cd ../data/experiments
-#./get_data.sh
+cd ../data/experiments
+./get_data.sh
 
-#mkdir KEEP THROW
-#machine=`hostname -s`
-#case $machine in
-#	rocks136)
-#        mv internet_usage.arff pendigits.arff vowel.arff KEEP
-#	;;
-#	rocks138)
-#        mv adult.arff communities.arff diabetes.arff KEEP
-#	;;
-#	rocks139)
-#        mv insurance.arff liver.arff shape.arff KEEP
-#	;;
-#	rocks141)
-#       mv breast.arff cover_type.arff glass.arff KEEP
-#	;;
-#esac
-#mv *.arff THROW
-#mv KEEP/* .
-#cd ../../experiments
+mkdir KEEP THROW
+machine=`hostname -s`
+case $machine in
+	rocks136)
+        mv internet_usage.arff pendigits.arff vowel.arff KEEP
+	;;
+	rocks138)
+        mv adult.arff communities.arff diabetes.arff KEEP
+	;;
+	rocks139)
+        mv insurance.arff liver.arff shape.arff KEEP
+	;;
+	rocks141)
+       mv breast.arff cover_type.arff glass.arff KEEP
+	;;
+esac
+mv *.arff THROW
+mv KEEP/* .
+cd ../../experiments
 
 R -f StrengthTest.R
 R -f ViewSelection.R
